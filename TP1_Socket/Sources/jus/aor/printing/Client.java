8791b87b2311814decae129b0e6f414945eeb26c
Talenie <alicia.abonnenc@hotmail.com>
2017-01-23 07:57:30 +0100

    TP1 premier commit

---
+++ TP1_Socket/Sources/jus/aor/printing/Client.java
@@ -0,0 +1,136 @@
+package jus.aor.printing;
+
+import java.net.*;
+import java.util.logging.Logger;
+import java.io.*;
+
+import jus.aor.printing.Client;
+import static jus.aor.printing.Notification.*;
+/**
+ * Représentation du Client du serveur d'impression.
+ * @author Morat
+ */
+public class Client {
+	/** 1 second timeout for waiting replies */
+	protected static final int TIMEOUT = 1000;
+	/** la machine supportant le serveur d'impression */
+	private String host = "localhost";
+	/** le port d'installation du serveur d'impression */
+	private int port=3000;
+	/** le listener UDP est-il vivant */
+	private boolean alive=true;
+	/** le logger du client */
+	private Logger log = Logger.getLogger("Jus.Aor.Printing.Client","jus.aor.printing.Client");
+	/** l'interfaçage avec la console du client */
+	private ClientGUI GUI;
+	/**
+	 * construction d'un client
+	 */
+	public Client() {
+		GUI = new ClientGUI(this);
+		log.setLevel(Level.INFO_2);
+	}
+	/**
+	 * Choix d'une imprimante distante en indiquant l'@ du servive d'impression associé :
+	 * se connecte à ce serveur d'impression, dès lors on peut envoyer des requêtes à celui-ci.
+	 */
+	public void selectPrinter() {
+		// constuction du listener de spooler
+		//----------------------------------------------------------------------------- A COMPLETER
+	}
+	/**
+	 * Se déconnecte d'un serveur d'impression, on ne peut plus envoyer de requête au serveur.
+	 */
+	public void deselectPrinter() {
+		// arrêt du listener de spooler
+		//----------------------------------------------------------------------------- A COMPLETER
+	}
+	/**
+	 * L'écoute des communications du spooler
+	 */
+	protected void runUDP(){
+		//----------------------------------------------------------------------------- A COMPLETER
+	}
+	/**
+	 * @param key la cle du du job
+	 * @throws IOException 
+	 */
+	private void jobTerminated(JobKey key) throws IOException{GUI.removePrintList(key);}
+	/**
+	 * Fixe le remote host
+	 * @param host le remote host supportant le serveur d'impression
+	 */
+	public void setHost(String host) {this.host=host;}
+	/**
+	 * Fixe le port du serveur d'impression
+	 * @param port le port supportant le serveur d'impression
+	 */
+	public void setPort(int port) {this.port=port;}
+	/**
+	 * protocole du print d'un fichier
+	 * @param f le fichier à imprimer
+	 */
+	private void onePrint(File f){
+		Socket server = null;
+		try(InputStream fis = new FileInputStream(f)){
+			Notification ret = null;
+                        server = new Socket(host,port);
+                        
+                        TCP.writeProtocole(server, QUERY_PRINT);
+                        
+                        JobKey j = new JobKey();
+                        TCP.writeJobKey(server, j);
+                        
+                        Notification retour = TCP.readProtocole(server);
+                        
+			if(retour == REPLY_PRINT_OK) {
+                            JobKey reponse = TCP.readJobKey(server);
+                                    
+                            System.out.println("jus.aor.printing.Client.onePrint()" + j.toString());
+                            System.out.println("jus.aor.printing.Client.onePrint()" + reponse.toString());
+                            if(!(j.equals(reponse))){
+                                log.log(java.util.logging.Level.SEVERE, "Mauvais JobKey reçu");
+                            }
+				//------------------------------------------------------------------------ A COMPLETER
+				// Dans le cas où tout est correct on ajoute le job à la liste des encours.
+				//	{log.log(Level.INFO_3,"Client.QueryPrint.Processing",key);
+				//	GUI.addPrintList(key);}
+			} else log.log(Level.WARNING,"Client.QueryPrint.Failed",ret.toString());
+                        
+                        
+		}catch(NumberFormatException e){
+			log.log(Level.SEVERE,"Client.QueryPrint.Port.Error",e.getMessage());
+		}catch(UnknownHostException e){
+			log.log(Level.SEVERE,"Client.QueryPrint.Remote.Error",e.getMessage());
+		}catch(IOException e){
+			log.log(Level.SEVERE,"Client.QueryPrint.IO.Error",e.getMessage()); 
+		}
+	}
+	/**
+	 * Réalise l'émission quasi-simultanée de n requêtes d'impression à l'aide de onePrint
+	 * @param f le fichier à imprimer
+	 * @param n nombre de requêtes d'impression à faire
+	 */
+	public void queryPrint(final File f,int n) {
+                onePrint(f);
+        }
+	/**
+	 * protocole du server status
+	 */
+	public void queryStatus(){		
+		//-------------------------------------------------------------------------- A COMPLETER
+	}
+	/**
+	 * protocole de délai d'impression
+	 * @param key la clé du job
+	 */
+	public void queryJobs(JobKey key){
+		//-------------------------------------------------------------------------- A COMPLETER	
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		new Client();
+	}
+}
---
+++ TP1_Socket/Sources/jus/aor/printing/Server.java
@@ -0,0 +1,114 @@
+package jus.aor.printing;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.Thread.State;
+import java.net.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+import jus.util.Formule;
+
+import static jus.aor.printing.Notification.*;
+/**
+ * Représentation du serveur d'impression.
+ * @author Morat
+ */
+public class Server {
+	/** 1 second timeout for waiting replies */
+	protected static final int TIMEOUT = 1000;
+	protected static final int MAX_REPONSE_LEN = 1024;
+	/** la taille de la temporisation */
+	protected int backlog =10;
+	/** le port de mise en oeuvre du service */
+	protected int port=3000;
+	/** le nombre d'esclaves maximum du pool */
+	protected int poolSize = 10;
+	/** le contrôle d'arret du serveur */
+	protected boolean alive = false;
+	/** le master server TCP socket */
+	protected ServerSocket serverTCPSoc;
+	/** le logger du server */
+	Logger log = Logger.getLogger("Jus.Aor.Printing.Server","jus.aor.printing.Server");
+	/**
+	 * Construction du server d'impression
+	 */
+	public Server() {
+		log.setLevel(Level.INFO_1);
+	}
+	/**
+	 * le master thread TCP.
+	 */
+	private void runTCP(){
+		try{
+			Socket client =null;
+			serverTCPSoc = new ServerSocket(port, backlog);
+			Notification protocole=null;
+			log.log(Level.INFO_1,"Server.TCP.Started",new Object[] {port,backlog});
+			while(alive) {
+				log.log(Level.INFO,"Server.TCP.Waiting");
+				try{
+                                    client = serverTCPSoc.accept();
+                                    protocole = TCP.readProtocole(client);
+                                    
+                                    JobKey j = TCP.readJobKey(client);
+                                    if(protocole == QUERY_PRINT){
+                                        TCP.writeProtocole(client, REPLY_PRINT_OK);
+                                        TCP.writeJobKey(client, j);
+                                    } else {
+                                        TCP.writeProtocole(client, REPLY_UNKNOWN_NOTIFICATION);
+                                    }
+                                    
+				}catch(SocketException e){
+						// socket has been closed, master serverTCP will stop.
+				}catch(ArrayIndexOutOfBoundsException e){
+					TCP.writeProtocole(client,REPLY_UNKNOWN_NOTIFICATION);
+				}catch(Exception e){
+					TCP.writeProtocole(client,REPLY_UNKNOWN_ERROR);
+				}
+			}
+			log.log(Level.INFO_1,"Server.TCP.Stopped");
+			serverTCPSoc.close();
+		}catch (Exception e){
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	protected void setBacklog(int backlog) {this.backlog=backlog;}
+	protected void setport(int port) {this.port=port;}
+	protected void setPoolSize(int poolSize) { this.poolSize=poolSize;}
+	/**
+	 * @param f
+	 * @see jus.aor.printing.Spooler#impressionTimeOfSize(jus.util.Formule)
+	 */
+//	public void impressionTimeOfSize(Formule f){spooler.impressionTimeOfSize(f);}
+	/**
+	 * 
+	 */
+	void start(){
+            alive = true;
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    runTCP();
+                }
+            });
+            t.start();
+        }
+	/**
+	 * 
+	 */
+	public void stop(){
+            alive = false;
+        }
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main (String args[]) { 
+            new ServerGUI(new Server()); 
+	}
+}
