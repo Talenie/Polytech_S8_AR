55ea83ed03eaff1e74e5eb8c3e3856fd58e26375
Talenie <alicia.abonnenc@hotmail.com>
2017-02-06 09:35:12 +0100

    automates et gestion des tailles de messages, pb dans les itérations

--- NIO-DONNE-ETD/Jus.Aor.Nio/Src.v3/jus/aor/nio/v3/ReadCont.java
+++ NIO-DONNE-ETD/Jus.Aor.Nio/Src.v3/jus/aor/nio/v3/ReadCont.java
@@ -5,25 +5,78 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 /**
- * @author morat 
+ * @author morat
  */
 public class ReadCont  extends Continuation{
-
-	// to complete
-
-	/**
-	 * @param sc
-	 */
-	public ReadCont(SocketChannel sc){
-		super(sc);
-	}
-	/**
-	 * @return the message
-	 * @throws IOException
-	 * @throws ClassNotFoundException 
-	 */
-	protected Message handleRead() throws IOException, ClassNotFoundException{
-	// todo
-	}
+    
+    // Etats de l'automate utilisé pour la lecture
+    private enum State {
+        IDLE, READING
+    };
+    
+    // Buffers pour lire la taille puis le message
+    private ByteBuffer size_buf = ByteBuffer.allocate(4);
+    private ByteBuffer message; // Alloué lorsque la taille est connue
+    
+    // Taille du message
+    private int size;
+    // Etat de l'automate
+    private State state;
+    
+    
+    /**
+     * @param sc
+     */
+    public ReadCont(SocketChannel sc){
+        super(sc);
+        state = State.IDLE;
+    }
+    /**
+     * Reads the message using an automata (first the size then the message)
+     * @return the message
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    protected Message handleRead() throws IOException, ClassNotFoundException{
+        Message msg = null;
+        switch(state){
+            case IDLE:
+                readsize();
+                break;
+            case READING :
+                msg = readmsg();
+            default:
+        }
+        return msg;
+    }
+    
+    /**
+     * Reads the size (on 4 bytes) and initializes the buffer for the message
+     * @throws IOException
+     */
+    private void readsize() throws IOException {
+        socketChannel.read(size_buf);
+        if(size_buf.remaining() == 0){
+            state = State.READING;
+            size = ReadCont.bytesToInt(size_buf);
+            message = ByteBuffer.allocate(size);
+        }
+    }
+    
+    /**
+     * Reads the message using the size
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private Message readmsg() throws IOException, ClassNotFoundException {
+        socketChannel.read(message);
+        if(message.remaining() == 0){
+            state = State.IDLE;
+            return new Message(message.array());
+        }
+        return null;
+    }
+    
 }
 
--- NIO-DONNE-ETD/Jus.Aor.Nio/Src.v3/jus/aor/nio/v3/WriteCont.java
+++ NIO-DONNE-ETD/Jus.Aor.Nio/Src.v3/jus/aor/nio/v3/WriteCont.java
@@ -7,51 +7,87 @@ import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 
 /**
- * @author morat 
+ * @author morat
  */
 public class WriteCont extends Continuation{
-	private SelectionKey key;
-	// state automata
-	private enum State{WRITING_DONE, WRITING_LENGTH,WRITING_DATA;}
-	// initial state
-	protected State state = State.WRITING_DONE;
-	// the list of bytes messages to write
-	protected ArrayList<byte[]> msgs = new ArrayList<>() ;
-	// buf contains the byte array that is currently written
-	protected ByteBuffer buf = null;
-
-
-	/**
-	 * @param k
-	 * @param sc
-	 */
-	public WriteCont(SelectionKey k,SocketChannel sc){
-		super(sc);
-		key = k;
-	}
-
-
-	/**
-	 * @return true if the msgs are not completly write.
-	 */
-	protected boolean isPendingMsg(){
-	// todo
-	}
-
-
-	/**
-	 * @param data
-	 * @throws IOException 
-	 */
-	protected void sendMsg(Message data) throws IOException{
-	// todo
-	}
-
-
-	/**
-	 * @throws IOException
-	 */
-	protected void handleWrite()throws IOException{
-	// todo
-	}
+    
+    private SelectionKey key;
+    // state automata
+    private enum State{WRITING_DONE,WRITING_LENGTH,WRITING_DATA;}
+    // initial state
+    protected State state = State.WRITING_DONE;
+    // the list of bytes messages to write
+    protected ArrayList<byte[]> msgs = new ArrayList<>() ;
+    // buf contains the byte array that is currently written
+    protected ByteBuffer buf = null;
+    
+    //Buffer containing the size
+    private ByteBuffer size_buf;
+       
+    
+    /**
+     * @param k
+     * @param sc
+     */
+    public WriteCont(SelectionKey k,SocketChannel sc){
+        super(sc);
+        key = k;
+    }
+    
+    
+    /**
+     * @return true if the msgs are not completly write.
+     */
+    protected boolean isPendingMsg(){
+        return !msgs.isEmpty();
+    }
+    
+    
+    /**
+     * @param data
+     * @throws IOException
+     */
+    protected void sendMsg(Message data) throws IOException{
+        msgs.add(data.marshall());
+        key.interestOps(SelectionKey.OP_WRITE|SelectionKey.OP_READ);
+    }
+    
+    
+    /**
+     * @throws IOException
+     */
+    protected void handleWrite()throws IOException{
+        switch(state){
+            case WRITING_DONE :
+                if(isPendingMsg()){
+                    buf = ByteBuffer.wrap(msgs.remove(0));
+                    state = State.WRITING_LENGTH;
+                }
+                break;
+            case WRITING_DATA :
+                writedata();
+                break;
+            case WRITING_LENGTH :
+                writelength();
+                
+            default :
+        }
+    }
+    
+    
+    private void writelength() throws IOException {
+        size_buf = Continuation.intToBytes(buf.remaining());
+        socketChannel.write(size_buf);
+        if(size_buf.remaining() == 0){
+            state = State.WRITING_DATA;
+        }
+    }
+    
+    private void writedata() throws IOException {
+        socketChannel.write(buf);
+        if(buf.remaining() == 0){
+            state = State.WRITING_DONE;
+            if(!isPendingMsg()) key.interestOps(SelectionKey.OP_READ);
+        }
+    }
 }
